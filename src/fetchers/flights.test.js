3d1d194f4306e1b52c9296743ca298d54f4e2ebc
Alexander Tsaplev <alexander@tsaplev.me>
2020-12-14 20:04:08 +0300

    Validate response schema

--- src/fetchers/flights.test.js
+++ src/fetchers/flights.test.js
@@ -1,15 +1,38 @@
 const { getAllFlights } = require("./flights");
+const { matchers } = require("jest-json-schema");
+
+expect.extend(matchers);
 
 describe("Fetching flights data", () => {
   test("fetch data from myflightradar", async () => {
     const flightsData = await getAllFlights("tsaplev");
+    const flightsSchema = {
+      type: "object",
+      required: ["points", "paths"],
+      additionalProperties: false,
+      properties: {
+        points: {
+          type: "array",
+          minItems: 1,
+          items: {
+            type: "object",
+            required: ["lat", "lon"],
+            additionalProperties: true,
+          },
+        },
+        paths: {
+          type: "array",
+          minItems: 1,
+          items: {
+            type: "array",
+            contains: { type: "string" },
+            minItems: 5,
+            maxItems: 5,
+          },
+        },
+      },
+    };
 
-    expect(flightsData).toHaveProperty("points");
-    expect(flightsData).toHaveProperty("paths");
-    flightsData.paths.forEach((path) => expect(path).toHaveLength(5));
-    flightsData.points.forEach((point) => {
-      expect(point).toHaveProperty("lat");
-      expect(point).toHaveProperty("lon");
-    });
+    expect(flightsData).toMatchSchema(flightsSchema);
   });
 });
