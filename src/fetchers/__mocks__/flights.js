44aea4be57be48f2608428a6206e15dd4664d8c2
Alexander Tsaplev <alexander@tsaplev.me>
2020-12-15 10:38:40 +0300

    Remove needless code

--- src/fetchers/__mocks__/flights.js
+++ src/fetchers/__mocks__/flights.js
@@ -1,8 +1,6 @@
 const flights = jest.requireActual("../flights");
 const { flightsData } = require("../__fixtures__/flights");
 
-jest.mock("./flights");
-
 const getAllFlights = jest.fn().mockReturnValue(flightsData);
 
 module.exports = {
--- src/fetchers/__mocks__/utils.js
+++ src/fetchers/__mocks__/utils.js
@@ -1,7 +1,5 @@
 const utils = jest.requireActual("../utils");
 
-jest.mock("./utils");
-
 const queryAllRows = jest.fn();
 
 module.exports = {
