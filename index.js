20f3d0e8abe09898abec38e5a0fde3f8237688c1
Alexander Tsaplev <alexander@tsaplev.me>
2020-12-29 23:42:04 +0300

    Add frontend components to entry point

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const Database = require("./src/fetchers/db");
 const { getTrips, getTripsByCountry } = require("./src/fetchers/trips");
 const { getFlights } = require("./src/fetchers/flights");
 
+const App = require("./src/components/app/index");
+const GoogleMaps = require("./src/components/maps/index");
+const CitiesLayout = require("./src/components/maps/layouts/cities");
+const FlightsLayout = require("./src/components/maps/layouts/flights");
+
 module.exports = {
   Database,
   getTrips,
   getTripsByCountry,
   getFlights,
+  App,
+  GoogleMaps,
+  CitiesLayout,
+  FlightsLayout,
 };
