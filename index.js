6804409dfeb44f661ec0dc630fa0effbfbf947b0
Alexander Tsaplev <alexander@tsaplev.me>
2020-12-30 00:06:58 +0300

    Add utils methods to entry point

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const Database = require("./src/fetchers/db");
 
-const { getTrips, getTripsByCountry } = require("./src/fetchers/trips");
+const {
+  getTrips,
+  getCitiesAsList,
+  getTripsByCountry,
+} = require("./src/fetchers/trips");
 const { getFlights } = require("./src/fetchers/flights");
+const { formatDate } = require("./src/fetchers/utils");
 
 const App = require("./src/components/app/index");
 const GoogleMaps = require("./src/components/maps/index");
@@ -11,8 +16,10 @@ const FlightsLayout = require("./src/components/maps/layouts/flights");
 module.exports = {
   Database,
   getTrips,
+  getCitiesAsList,
   getTripsByCountry,
   getFlights,
+  formatDate,
   App,
   GoogleMaps,
   CitiesLayout,
